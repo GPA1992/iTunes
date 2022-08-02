16b4687c19b02bbb008bc49b861f5825e55f9cf9
Gabriel <gabrielproencaaraujo@gmail.com>
2022-08-02 17:40:33 -0300

    Item 05

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state= {
+    enableButton: true,
+  }
+
+  textCount = ({ target }) => {
+    const { value } = target;
+    const minCaractere = 2;
+    const text = value.length < minCaractere;
+    this.setState({ enableButton: text });
+  }
+
   render() {
+    const { enableButton } = this.state;
     return (
-      <div data-testid="page-search">
+      <div>
         <Header />
-        <div>search</div>
+        <div data-testid="page-search">
+          <form action="">
+            <input
+              onChange={ this.textCount }
+              type="text"
+              data-testid="search-artist-input"
+            />
+            <button
+              disabled={ enableButton }
+              type="submit"
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
