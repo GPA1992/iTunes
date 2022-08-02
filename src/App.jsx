e2d8710e53ed12aef21f3072b89a87e20de946bf
Gabriel <gabrielproencaaraujo@gmail.com>
2022-08-02 08:51:02 -0300

    Exercicio 1

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Search from './Pages/Search';
+import Album from './Pages/Album';
+import Favorites from './Pages/Favorites';
+import Profile from './Pages/Profile';
+import Edit from './Pages/Profile/Edit';
+import NotFound from './Pages/NotFound';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ Edit } />
+          <Route path="" component={ NotFound } />
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
