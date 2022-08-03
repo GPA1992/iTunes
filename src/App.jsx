1f064374e0823eab6f10858003e214f6fb6dd02d
Gabriel <gabrielproencaaraujo@gmail.com>
2022-08-02 22:02:54 -0300

    Código Refatorado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
 import Search from './Pages/Search';
 import Album from './Pages/Album';
@@ -7,53 +7,15 @@ import Favorites from './Pages/Favorites';
 import Profile from './Pages/Profile';
 import Edit from './Pages/Profile/Edit';
 import NotFound from './Pages/NotFound';
-import { createUser } from './services/userAPI';
 
 class App extends React.Component {
-    state = {
-      name: '',
-      disableEnter: true,
-      loged: false,
-      onLoading: false,
-    };
-
-  getName = ({ target }) => {
-    const { value } = target;
-    const minCaractere = 3;
-    const enableEnter = value.length < minCaractere;
-    this.setState({
-      name: value,
-      disableEnter: enableEnter,
-    });
-  }
-
-  funcCreateUser = async () => {
-    const { name } = this.state;
-    this.setState({ onLoading: true });
-    await createUser({ name });
-    this.setState({
-      loged: true,
-      onLoading: true,
-    });
-  }
-
   render() {
-    const { disableEnter, loged, onLoading } = this.state;
     return (
       <div>
         <BrowserRouter>
           <Switch>
             <Route exact path="/search" component={ Search } />
-            {loged ? <Redirect to="/search" /> : <Route
-              exact
-              path="/"
-              render={ () => (<Login
-                disableEnter={ disableEnter }
-                getName={ this.getName }
-                funcCreateUser={ this.funcCreateUser }
-                onLoading={ onLoading }
-              />) }
-            />}
+            <Route exact path="/" component={ Login } />
             <Route exact path="/album/:id" component={ Album } />
             <Route exact path="/favorites" component={ Favorites } />
             <Route exact path="/profile" component={ Profile } />
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,40 +1,70 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+/* import PropTypes from 'prop-types'; */
+import { Redirect } from 'react-router-dom';
 import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    onLoading: false,
+    name: '',
+    disableEnter: true,
+    loged: false,
+  }
+
+  getName = ({ target }) => {
+    const { value } = target;
+    const minCaractere = 3;
+    const enableEnter = value.length < minCaractere;
+    this.setState({
+      disableEnter: enableEnter,
+      name: value,
+    });
+  }
+
+  funcCreateUser = async () => {
+    const { name } = this.state;
+    this.setState({ onLoading: true });
+    await createUser({ name });
+    this.setState({
+      loged: true,
+      onLoading: false,
+    });
+  }
+
   render() {
-    const { funcCreateUser, getName, disableEnter, onLoading } = this.props;
+    const { loged, onLoading, disableEnter } = this.state;
     return (
       <div>
-        { onLoading ? (<Loading />
+        { loged ? (<Redirect to="/search" />
         ) : (
           <div data-testid="page-login">
             <input
               data-testid="login-name-input"
-              onChange={ getName }
+              onChange={ this.getName }
               type="text"
               testid="login-name-input"
               placeholder="Nome"
             />
             <button
               disabled={ disableEnter }
-              onClick={ funcCreateUser }
+              onClick={ this.funcCreateUser }
               type="button"
               data-testid="login-submit-button"
             >
               Entrar
             </button>
           </div>)}
+        { onLoading && (<Loading />)}
       </div>
     );
   }
 }
 
-Login.propTypes = {
+/* Login.propTypes = {
   funcCreateUser: PropTypes.func.isRequired,
   getName: PropTypes.func.isRequired,
   disableEnter: PropTypes.bool.isRequired,
   onLoading: PropTypes.bool.isRequired,
-};
+}; */
 export default Login;
